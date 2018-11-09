2c7ebc55dcf456051ce2cd56c5975a35900d204f
assasinfil <nfa57643@gmail.com>
2018-11-09 22:04:12 +0300

    fix your upload

--- godgame/godgame/MainWindow.xaml.cs
+++ godgame/godgame/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace godgame
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<string> whichlist = new List<string>();
+        List<string> wholist = new List<string>();
+        List<string> whoselist = new List<string>();
+        List<string> inwitchlist = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
+            which.ItemsSource = whichlist;
+            who.ItemsSource = wholist;
+            whose.ItemsSource = whoselist;
+            inwitch.ItemsSource = inwitchlist;
+        }
+
+        private void addwhich_Click(object sender, RoutedEventArgs e)
+        {
+            whichlist.Add(saveline.Text);
+            saveline.Text = "";
+            which.Items.Refresh();
+        }
+
+        private void addwho_Click(object sender, RoutedEventArgs e)
+        {
+            wholist.Add(saveline.Text);
+            saveline.Text = "";
+            who.Items.Refresh();
+        }
+
+        private void addinwitch_Click(object sender, RoutedEventArgs e)
+        {
+            inwitchlist.Add(saveline.Text);
+            saveline.Text = "";
+            inwitch.Items.Refresh();
+        }
+
+        private void addwhose_Click(object sender, RoutedEventArgs e)
+        {
+            whoselist.Add(saveline.Text);
+            saveline.Text = "";
+            whose.Items.Refresh();
         }
     }
 }
